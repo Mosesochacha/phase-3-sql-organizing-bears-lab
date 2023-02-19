0e563d2a3d6b9efa04f12bd08142a7f56903a16b
mosesochacha <ochachamoses443@@gmail.com>
2023-02-19 22:25:40 +0300

    done

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE
+  IF NOT EXISTS bears (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN,
+  );
